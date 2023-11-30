993e42f320b60486e44a7e180016aa989b70533c
xuxuxe <exlatedorb@gmail.com>
2023-11-30 17:21:29 +0300

    some more

--- src/components/SkeletonTitle/SkeletonTitle.tsx
+++ src/components/SkeletonTitle/SkeletonTitle.tsx
@@ -3,6 +3,30 @@ import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 function SkeletonTitle() {
+  if (window.innerWidth < 1100) {
+    return (
+      <SkeletonTheme baseColor="#05315A" highlightColor="#0C4281 ">
+        <div className="title-main skeleton-title" style={{ width: "260px" }}>
+          <div className="title-poster-wraper">
+            <div className="title-poster poster">
+              <Skeleton height="100%" className="title-poster poster" />
+            </div>
+          </div>
+          <div className="title-name-tags">
+            <span className="title-name name" style={{ marginBottom: "10px" }}>
+              <Skeleton width={135} height={40} />
+            </span>
+            <span className="title-episodes">
+              <Skeleton width={120} />
+            </span>
+            <div className="title-tags tags">
+              <Skeleton width={120} />
+            </div>
+          </div>
+        </div>
+      </SkeletonTheme>
+    );
+  }
   return (
     <SkeletonTheme baseColor="#05315A" highlightColor="#0C4281">
       <div className="catalogue-page-title title-main skeleton-title">
